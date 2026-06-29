db598dcd14b8c0e899463273957af493d2254148
wilyJ80 <abjurandam@gmail.com>
2026-06-29 00:04:40 -0300

    feat: finish 1 introducao monografia

--- tcc1/monografia/doc/compile.sh
+++ tcc1/monografia/doc/compile.sh
@@ -2,4 +2,11 @@
 
 # Template sadly only works with TeXLive distribution, with glossary fixes
 
+# Compile doc
+pdflatex monografia.tex
+# Process references
+bibtex monografia
+# Place citations
+pdflatex monografia.tex
+# Final compile
 pdflatex monografia.tex
