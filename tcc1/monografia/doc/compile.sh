433f00d6ec7b35029102469577171831ed43b56b
wilyJ80 <abjurandam@gmail.com>
2026-07-13 08:46:29 -0300

    feat: config figuras

--- tcc1/monografia/doc/compile.sh
+++ tcc1/monografia/doc/compile.sh
@@ -12,3 +12,10 @@ bibtex monografia
 pdflatex monografia.tex
 # Final compile
 pdflatex monografia.tex
+
+# Workaround: remover a maldita pagina 10 que o LaTeX insiste em criar
+if [ -f monografia.pdf ]; then
+    echo "Removendo a pagina 10 do PDF..."
+    pdftk monografia.pdf cat 1-9 11-end output monografia_temp.pdf
+    mv monografia_temp.pdf monografia.pdf
+fi
